7001a266a76b3be55e4277a0378cdfff6972cce1
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-10-30 22:13:16 +0700

    Add files via upload

---
+++ PBOIF2-10119083-Latihan18-kelipatan/src/pboif2/pkg10119083/latihan18/kelipatan/PBOIF210119083Latihan18Kelipatan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan18.kelipatan;
+
+/**
+ *
+ * @author
+ * Nama : Rafsan Zen Mustaofa
+ * NIM  : 10119083
+ * Kelas: IF-2
+ * Deskripsi: Menamplkan kelipatan
+ */
+public class PBOIF210119083Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            for (double i = 3.5; i <= 35; i+=3.5) 
+                System.out.println(i);
+    System.out.println("==============================");
+    System.out.println("Developed by : Rafsan Zen Mustaofa");
+    }  
+    
+}
